b28f4825ff7bf7a9b43ba574a2688472cffce654
SujithAcharya99 <52646806+SujithAcharya99@users.noreply.github.com>
2020-11-23 16:35:37 +0530

    Update users.js

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,13 +1,10 @@
 const users = [];
-
-
 //adduser, removeUser, Getuser, getusersinroom
 
 const addUser = ({ id, username, room }) => {
     //clear the data 
     username = username.trim().toLowerCase(),
     room = room.trim().toLowerCase()
-
 
     //validate the data
     if (!username || !room) {
@@ -32,7 +29,6 @@ const addUser = ({ id, username, room }) => {
      const user = { id, username, room }
      users.push(user)
      return { user }
-
 }
 
 const removeUser = (id) => {
@@ -52,42 +48,6 @@ const getusersInRoom = (room) => {
     return users.filter((user) => user.room === room)
 }
 
-// addUser({
-//     id: 22,
-//     username: 'sujith',
-//     room: 'odepoi'
-// })
-
-// addUser({
-//     id: 23,
-//     username: 'sanjay',
-//     room: 'odepoi'
-// })
-// addUser({
-//     id: 12,
-//     username: 'sharath',
-//     room: 'ijes'
-// })
-// // console.log(users)
-
-// const res = addUser({
-//     id:22,
-//     username: 'sujith',
-//     room:'odepoi'
-// })
-
-// const removedUsers = removeUser(22);
-
-// console.log(removedUsers);
-
-// console.log(users)
-
-// const user = getUser(12)
-// console.log(user)
-
-// const userlist = getusersInRoom('iqjes')
-
-// console.log(userlist)
 module.exports = {
     addUser,
     removeUser,
